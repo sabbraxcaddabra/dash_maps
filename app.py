51e77443f20ff724b164dd7d280b74e8f1821b7d
sabbraxcaddabra <sabbraxcaddabra@yandex,ru>
2022-11-16 21:35:11 +0000

    Сделали карту и клики, начинатся отладка

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from dash import dcc, html, Output, Input, State
 import dash_bootstrap_components as dbc
 import os
 import pandas as pd
+import json
+
 import dash_leaflet as dl
+from dash_extensions.javascript import arrow_function
 
 HERE = os.path.dirname(__file__)
 REGIONS_FILE = os.path.abspath(os.path.abspath(os.path.join(HERE, ".", "regi.csv")))
@@ -11,6 +14,10 @@ CITIES_FILE = os.path.abspath(os.path.abspath(os.path.join(HERE, ".", "cities.cs
 
 reg_df = pd.read_csv(REGIONS_FILE)
 city_df = pd.read_csv(CITIES_FILE)
+
+
+with open('geodata/russia_2021.geojson', 'r', encoding='utf-8') as file:
+    data = json.load(file)
 
 def generate_region_total_layout_enrolled(region_dict: dict):
     '''
@@ -43,6 +50,7 @@ def generate_region_cities_layout_enrolled(region_c_df: pd.DataFrame):
         data=region_c_df.to_dict('records'),
         columns=[{"name": i, "id": i} for i in region_c_df.columns],
         style_cell={'textAlign': 'left'},
+        sort_action="native",
         page_size=10
     )
 
@@ -92,34 +100,40 @@ app.layout = dbc.Container(children=[
         dbc.ModalHeader(dbc.ModalTitle(id='regio_modal_title')),
         dbc.ModalBody(id='regio_modal_body')
     ], id='regio_modal', fullscreen=True),
-    dbc.Button(id='submit', children='Отобразить статистику'),
+    html.Br(),
+    dl.Map(center=[65, 70], zoom=3, children=[dl.TileLayer(url='https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png'),
+                                              dl.GeoJSON(data=data, id="states",
+                                                         hoverStyle=arrow_function(dict(weight=5, color='#666', dashArray='')))
+                                              ], style={'width': '100%', 'height': '90vh', 'margin': "auto", "display": "block"}, id="map"),
+    html.Div(id="state"), html.Div(id="capital")
 ], fluid='sm')#style={'marginLeft': 250, 'marginRight': 250})
 
 @app.callback(
     [Output("regio_modal", "is_open"), Output("regio_modal_title", 'children'), Output('regio_modal_body', 'children')],
-    [Input('submit', "n_clicks")],
+    [Input("states", "click_feature")],
     [State('regio_modal', 'is_open'), State("type_stat", "value")]
 )
-def show_modal(n, is_open, type_stat):
-    if n is None:
+def show_modal(feature, is_open, type_stat):
+    if not feature:
         raise dash.exceptions.PreventUpdate
-    else:
-        if type_stat == 2:
-            reg_stats = get_region_stats_by_id_enrolled(59)
-            reg_name = reg_stats['Название региона']
-            del reg_stats['Название региона']
-            reg_total_table = generate_region_total_layout_enrolled(reg_stats)
-            reg_c_df = get_region_cities_stats_by_id_enrolled(59)
-            reg_c_table = generate_region_cities_layout_enrolled(reg_c_df)
-            lout = html.Div(
-                children=[
-                    reg_total_table,
-                    html.Br(),
-                    html.H4('Статистика по городам'),
-                    reg_c_table
-                ]
-            )
-            return not is_open, reg_name, lout
+
+    reg_id = feature['properties']['id']
+    if type_stat == 2:
+        reg_stats = get_region_stats_by_id_enrolled(reg_id)
+        reg_name = reg_stats['Название региона']
+        del reg_stats['Название региона']
+        reg_total_table = generate_region_total_layout_enrolled(reg_stats)
+        reg_c_df = get_region_cities_stats_by_id_enrolled(reg_id)
+        reg_c_table = generate_region_cities_layout_enrolled(reg_c_df)
+        lout = html.Div(
+            children=[
+                reg_total_table,
+                html.Br(),
+                html.H4('Статистика по городам'),
+                reg_c_table
+            ]
+        )
+        return not is_open, reg_name, lout
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
